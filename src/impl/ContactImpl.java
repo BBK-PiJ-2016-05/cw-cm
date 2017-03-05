2b51fb930d5b5058fe413cb7dfbcfaeeb43dd4b8
Duncan <Duncan>
2017-03-05 09:57:15 +0000

    Fixing ContactImpl to pass my first test (which it did)

--- src/impl/ContactImpl.java
+++ src/impl/ContactImpl.java
@@ -7,28 +7,36 @@ import spec.Contact;
  * Implements the Contact interface
  */
 public class ContactImpl implements Contact {
+
+    private String name;
+    private String notes;
+    private int id;
+
     /**
-     * most general constructor with three parameters:
-     * @param ID the ID provided by the ContactManager
+     * most general constructor where the ID is provided by the ContactManager
      * @param name the name
      * @param initialNotes the initial set of notes about the contact
      * @throws IllegalArgumentException if the ID provided is zero or negative
      * @throws NullPointerException if any of the references / pointers passed as
      *      parameters to the constructor is null
      */
-    ContactImpl(int ID, String name, String initialNotes){
-
+    public ContactImpl(String name, String initialNotes){
+        this.name = name;
+        this.notes = initialNotes;
+        //this.id = myContactManager.addNewContact(name, initialNotes);
     }
 
     /**
-     * more restricted constructor with two parameters only:
-     * @param ID the ID provided by the ContactManager
+     * more restricted constructor where the ID is provided by the ContactManager
      * @param name the name
      * @throws IllegalArgumentException if the ID provided is zero or negative
      * @throws NullPointerException if any of the references / pointers passed as
      *      parameters to the constructor is null
      */
-    ContactImpl(int ID, String name){
+    public ContactImpl(String name){
+        this.name = name;
+        this.notes = "";
+        //this.id = myContactManager.addNewContact(name, initialNotes);
 
     }
 
